ac4a18bc28da5521d6a8c0782aa6435191d76188
sdpeyton <stephendpeyton@gmail.com>
2017-11-07 22:46:04 -0500

    added layout as directory to hold general structure components, started with navbar

---
+++ src/layout/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './Navbar.css';
+
+const Navbar = () => (
+    <div id='navbar'>
+        <span>Contact List App</span>
+    </div>
+);
+
+export default Navbar;
